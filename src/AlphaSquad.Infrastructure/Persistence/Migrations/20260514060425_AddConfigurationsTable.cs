6e02017ec2961dcc16bab53648b70f2b7d3fd4ca
Marcelo Oliveira <maclauware@hotmail.com>
2026-05-14 03:07:14 -0300

    Nova migration de configuracoes

---
+++ src/AlphaSquad.Infrastructure/Persistence/Migrations/20260514060425_AddConfigurationsTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlphaSquad.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfigurationsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CheckInPassword",
+                table: "academy_events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "academy_events",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPresent",
+                table: "academy_event_participations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckInPassword",
+                table: "academy_events");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "academy_events");
+
+            migrationBuilder.DropColumn(
+                name: "IsPresent",
+                table: "academy_event_participations");
+        }
+    }
+}
